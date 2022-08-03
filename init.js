1d6b2f4ac56cf13c71c606cbb24cc3d05f6f036b
lublak <lublak.de@gmail.com>
2022-08-03 16:27:30 +0200

    move more template based files

--- init.js
+++ init.js
@@ -27,9 +27,22 @@ async function apply(file) {
   data = fillReplace(data, 'REPO_FULLNAME', repo);
   fs.writeFile(file, data);
 }
+
+/**
+ * moves a .template file
+ * @param {string} file
+ */
+async function moveTemplate(file) {
+  await fs.unlink(file).catch(() => {});
+  await fs.rename(file + '.template', 'README.md');
+}
+
 async function init() {
-  await fs.unlink('README.md');
-  await fs.rename('README.template.md', 'README.md');
+  await moveTemplate('README.md');
+  await moveTemplate('.github/ISSUE_TEMPLATE/bug_report.yml');
+  await moveTemplate('.github/ISSUE_TEMPLATE/feature_request.yml');
+  await moveTemplate('.github/worklows/node.js.yml');
+  await moveTemplate('.github/worklows/npm-publish.yml');
   await apply('README.md');
   await apply('package.json');
   await apply('package-lock.json');
